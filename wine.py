eb2c05f42e4d402ada9ef280f011874f21c702ab
Ramiro <pfcperez@gmail.com>
2019-02-17 20:08:33 -0600

    Adding new files

---
+++ wine.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.externals import joblib
+
+dataset_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
+data = pd.read_csv(dataset_url, sep=';')
+
+print(data.head())
+print (data.shape)
+print(data.describe())
+
+y = data.quality
+X = data.drop('quality', axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123, stratify=y)
+
+X_train_scaled = preprocessing.scale(X_train)
+print(X_train_scaled)
+
+print (X_train_scaled.mean(axis=0))
+
+print (X_train_scaled.std(axis=0))
